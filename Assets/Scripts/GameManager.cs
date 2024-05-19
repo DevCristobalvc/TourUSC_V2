5d945737e033048987538e45ec6f0ceb46f51e66
Sebastian <sebastianwho1776@gmail.com>
2024-05-19 15:42:22 -0500

    Recorridos Creados
    Creados los Recorridos para La Fuente, Bloque 1 y Bloque 4

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //Variable de la Esfera
     [SerializeField] private MeshRenderer sphereRenderer;
+    //Array que contiene las "Escenas"
     [SerializeField] private SceneContainer[] containers;
+    //Plataforma de Inicio
     [SerializeField] private GameObject startMenu;
 
+    //Identificador del momento actual del Recorrido
     private string currentSphere;
+    //Identificador de la escena actual
     private SceneContainer currentScene;
 
+    //Funcion que retorna una Escena basado en su id
     public SceneContainer GetSceneContainer(string id) => containers.FirstOrDefault(c => c.sceneId == id);
+    
+    //Funcion que retorna si existe una Escena con el ID dado
     public bool SceneExists(string id) => containers.Any(c => c.sceneId == id);
 
+    //Funcion que retorna un momento del Recorrido basado en la escena y el ID dado
     public SphereContainer GetSphereContainer(SceneContainer scene, string id) =>
         scene.container.FirstOrDefault(c => c.id == id);
 
+    //Funcion que retorna si un momento del Recorrido existe basado en su escena y el ID dado
     public bool SphereExists(SceneContainer scene, string id) => scene.container.Any(c => c.id == id);
     
+    //Funcion para cambiar la Escena con un ID
     public void SelectScene(string id)
     {
         if (!SceneExists(id)) return;
@@ -30,6 +41,7 @@ public class GameManager : MonoBehaviour
         SelectSphere(currentScene.container[0].id);
     }
 
+    //Funcion para cambiar el momento del Recorrido con su ID
     public void SelectSphere(string id)
     {
         if (!SphereExists(currentScene, id)) return;
@@ -44,17 +56,30 @@ public class GameManager : MonoBehaviour
     }
 }
 
+/*
+    Clase que contiene las Escenas. Una Escena es considerada como uno de los recorridos.
+    Ejemplo: Una escena es el Bloque 1, que dentro de si contiene los momentos o partes del recorrido
+ */
 [System.Serializable]
 public class SceneContainer
 {
+    //Identificador de la Escena
     public string sceneId;
+    //Array de momentos/partes del recorrido
     public SphereContainer[] container;
 }
 
+/*
+    Clase que contiene los Momentos/Partes del recorrido. Estas son todas las imagenes o partes
+    del recorrido que se hace
+ */
 [System.Serializable]
 public class SphereContainer
 {
+    //Identificador del Momento/Parte
     public string id;
+    //Identificador de la Textura/Material
     public Material sphereMaterial;
+    //Botones de Recorrido a Activar
     public GameObject[] sphereObjects;
 }
